3367cfee07c86caee656a0b7ece55623a9944d93
kent013 <kentaro.ishitoya@gmail.com>
2018-09-20 09:05:06 +0900

    4.2

--- Tests/TabPageViewControllerTests/TabPageViewControllerTests.swift
+++ Tests/TabPageViewControllerTests/TabPageViewControllerTests.swift
@@ -3,9 +3,6 @@ import XCTest
 
 class TabPageViewControllerTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(TabPageViewController().text, "Hello, World!")
     }
 
 
